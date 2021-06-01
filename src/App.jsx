0a0da0cab2ec933c9c3f71f1b362b2c4df987722
Andre-bled <42767273+Andre-bled@users.noreply.github.com>
2021-06-01 16:24:06 +0200

    add students variable below assignments

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ class App extends React.Component {
 
     this.state = {
       buttonClicked: "",
-      assignments: [] /*Below this line, add the students state variable*/,
+      assignments: [],
+      students: [],
       grades: {},
-      students: []
     };
 
     this.handleButtonClicked = this.handleButtonClicked.bind(this);
